8dabbf343e0e9f4a47ad1e715cfbcc5337e6db27
kessyy <kather946@gmail.com>
2020-06-16 08:49:22 +0300

    changes

--- styles/index.js
+++ styles/index.js
@@ -3,3 +3,6 @@ const copyrightYear = document.querySelector('.year');
 const date = new Date();
 const year = date.getFullYear();
 copyrightYear.innerText = year;
+
+const input = document.querySelector('input');
+function subcribeNow(){input.value = "";}
